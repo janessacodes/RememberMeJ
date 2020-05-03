043ccf6d0fec575fe76bc938590eb0517b75917d
janessacodes <59347196+janessacodes@users.noreply.github.com>
2020-05-03 15:20:31 -0700

    Add files via upload

---
+++ RememberMe/ScrollTimeline.swift
@@ -0,0 +1,30 @@
+//
+//  ScrollTimeline.swift
+//  RememberMe
+//
+//  Created by Janessa Ulug on 5/3/20.
+//  Copyright © 2020 Sriya Kantipudi. All rights reserved.
+//
+
+import UIKit
+
+class ScrollTimeline: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
